013338cc9c11641baab7a5405bc2a8205990b2fb
Ahmet Emre Sağcan <mrsagcan@yahoo.com>
2021-12-15 20:44:13 +0300

    Enemy script adds colliders

--- 4_Planetary_Strike/Assets/Scripts/Enemy.cs
+++ 4_Planetary_Strike/Assets/Scripts/Enemy.cs
@@ -1,7 +1,15 @@
+using System;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    private Collider collider;
+    private void Start()
+    {
+        collider = gameObject.AddComponent<BoxCollider>();
+        collider.isTrigger = false;
+    }
+
     private void OnParticleCollision(GameObject other)
     {
         Destroy(gameObject);
