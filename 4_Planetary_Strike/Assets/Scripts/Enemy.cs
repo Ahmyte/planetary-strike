fa7a6d591010f39cb4d8410838c9fc565d2a48df
Ahmet Emre Sağcan <mrsagcan@yahoo.com>
2022-02-13 14:16:39 +0300

    Enemies have maximum hits

--- 4_Planetary_Strike/Assets/Scripts/Enemy.cs
+++ 4_Planetary_Strike/Assets/Scripts/Enemy.cs
@@ -7,6 +7,7 @@ public class Enemy : MonoBehaviour
     [SerializeField] private Transform parentDirectory;
     
     [SerializeField] private int scorePerHit = 12;
+    [SerializeField] private int hits = 3;
     private ScoreBoard scoreBoard;
     private void Start()
     {
@@ -22,8 +23,17 @@ public class Enemy : MonoBehaviour
 
     private void OnParticleCollision(GameObject other)
     {
-        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
         scoreBoard.ScoreHit(scorePerHit);
+        hits--;
+        if (hits < 1)
+        {
+            KillEnemy();
+        }
+    }
+
+    private void KillEnemy()
+    {
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
         fx.transform.parent = parentDirectory;
         Destroy(gameObject);
     }
